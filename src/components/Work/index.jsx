370ce72b934c982f6c0b138da8e55e9501b5f196
PHTMGatt <phtmgatt@gmail.com>
2025-03-11 22:14:58 -0400

    added portfolio to work folder

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Work = ({ screenshot, description, link }) => {
+const WorkItem = ({screenshot, description, link}) => {
   return (
-    <div className='grid-item'>
-      <img src={screenshot} alt={description} className='screenshot' />
-      <h3>{description}</h3>
-      <a href={link} target='_blank' rel='noopener noreferrer'>
-        View Project
-      </a>
-    </div>
+    <div className="grid-item">
+    <img src={screenshot} alt={description} className="screenshot" />
+    <h3>{description}</h3>
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      View Project
+    </a>
+  </div>
   )
 }
 
-export default Work
+export default WorkItem
