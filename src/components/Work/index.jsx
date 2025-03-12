6fd40b763506c3906318370e019d4937062c9cf7
PHTMGatt <phtmgatt@gmail.com>
2025-03-11 20:21:25 -0400

    updated work index

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Work = () => {
+const Work = ({ screenshot, description, link }) => {
   return (
-    <div>Work</div>
+    <div className='grid-item'>
+      <img src={screenshot} alt={description} className='screenshot' />
+      <h3>{description}</h3>
+      <a href={link} target='_blank' rel='noopener noreferrer'>
+        View Project
+      </a>
+    </div>
   )
 }
 
